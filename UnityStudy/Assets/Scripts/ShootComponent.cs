85089358760fcaa10f8336fe5596a7a87be21549
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-05-09 21:14:26 +0900

    shootComponent && weaponComponent 제작

---
+++ UnityStudy/Assets/Scripts/ShootComponent.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootComponent : MonoBehaviour
+{
+    [SerializeField] WEAPON wType = WEAPON.NORMAL;  //현재 무기 타입
+    [SerializeField] GameObject[] weapon;           //생성할 무기 오브젝트 배열
+    GameObject currentWeapon;                       //현재 무기를 담아둘 변수
+    WeaponComponent wc;                             //현재 무기의 WeaponComponent를 담아둠
+
+    private void Start()
+    {
+        //SetWeapon(WEAPON.NORMAL);
+    }
+
+    void Update()
+    {
+        //if (Input.GetMouseButton(0)){
+        //    wc.Shot(); //발사
+        //}
+    }
+
+    public void SetWeapon(WEAPON _t) //무기 바꿀때 호출
+    {
+        wType = _t;
+        if (currentWeapon) //currentWeapon이 null인지 아닌지 판단
+        {
+            Destroy(currentWeapon); //null이 아니라면 삭제
+        }
+
+        currentWeapon = Instantiate(weapon[(int)_t], transform.position, Quaternion.identity, transform);
+        //_t에 해당하는 무기를 자식으로 생성
+    }
+}
---
+++ UnityStudy/Assets/Scripts/WeaponComponent.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public enum WEAPON //무기 종류 => 열거문으로 선언 
+{
+    NORMAL,
+    FLAME,
+    SHOTGUN,
+    E_LASER
+}
+public abstract class WeaponComponent : MonoBehaviour  //추상 클래스로 선언
+{
+    [SerializeField] protected int atk;                //공격력
+    [SerializeField] protected float rate;             //공격속도
+    [SerializeField] protected float cri;              //크리티컬 확률
+   
+
+    [SerializeField] protected WEAPON wType;          //무기 종류
+    protected bool canShoot = true;                   //발사 가능 판단 변수
+    [SerializeField] int ammo = 0;                    //총알량
+
+    public int AMMO { get { return ammo; } }          //현재 남은 탄약 발수
+    //get으로 바로 ammo를 반환해서 옴
+
+    public abstract void Shot();                     //재정의할 함수
+                                                     //총의 종류마다 쏘는 방법만 다르기 때문에
+                                                     //추상클래스로 상속받아서 사용할 예정
+    
+    
+    protected IEnumerator ShotRate() //공격속도에 따른 딜레이 추가
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(rate);
+        canShoot = true;
+    }
+    private void OnTriggerEnter(Collider other) //무기를 이펙트로 설정하기 때문에 몬스터와 무기가 충돌 시 데미지를 주도록 설정
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            DamageEnemy(other.gameObject); //데미지 주는 부분
+        }
+    }
+
+    protected void DamageEnemy(GameObject obj) // 크리티컬에 따라 데미지 증가 => 데미지 주는 부분
+    {
+        int dmg = atk; //데미지 <나중에 공격력 증가 적용을 위해 따로 선언>
+        if(cri >= Random.Range(0f, 100f)) //크리티컬 터질 때
+        {
+            dmg *= 2;   //데미지 두배
+        }
+        
+        //obj로 받은 객체의 TakeDamage(dmg) 함수 호출
+    }
+
+    protected void ReduceAmmo()
+    {
+        ammo--;
+    }
+}
