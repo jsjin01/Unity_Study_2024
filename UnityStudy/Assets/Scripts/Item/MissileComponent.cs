30bfaf2d23df904ec30d37bd98f3aabccef81b86
pm2979 <pm29798512@gmail.com>
2024-05-19 22:01:15 +0900

    pm

---
+++ UnityStudy/Assets/Scripts/Item/MissileComponent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileComponent : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float speed = 5; //미사일 속도
+
+    public void MissileMove(Vector3 enemy)
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = enemy * speed; // enemy 방향으로 이동
+    }
+
+    private void OnTriggerEnter(Collider other) // 충동 감지
+    {
+        if(other.CompareTag("Enemy")) // Tag가 "Enemy"일 경우
+        {
+            Destroy(gameObject);  //오브젝트 삭제
+        }
+    }
+}
