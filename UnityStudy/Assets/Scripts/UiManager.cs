e7c1f9151ab4fb040b70394c0253971daacc6941
seohyun0030 <163545492+seohyun0030@users.noreply.github.com>
2024-06-19 19:43:57 +0900

    경험치바

--- UnityStudy/Assets/Scripts/UiManager.cs
+++ UnityStudy/Assets/Scripts/UiManager.cs
@@ -8,6 +8,8 @@ public class UiManager : MonoBehaviour
 {
     public static UiManager i;
 
+    public Slider expSlider;
+
     private void Awake()
     {
         i =this;
@@ -20,10 +22,23 @@ public class UiManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        expBar();
     }
 
     public void hpBar(float hp)
     {
         GameObject.Find("Hp").GetComponent<Slider>().value = hp;
+    }
+
+    public void expBar()
+    {
+        expSlider.value = PlayerManager.i.exp;
+        
+        if(expSlider.value >= 100)  //경험치바 100이상이면
+        {
+            expSlider.value = 0;    //슬라이더 초기화
+            PlayerManager.i.exp = 0;    //경험치 초기화
+            CardManager.i.SelectedCard();   //카드뽑기
+        }
     }
 }
